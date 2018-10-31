3dede410442785fd707b5f219c0895fd62ba24b3
John Velis <john@johnvelis.com>
2018-10-31 12:43:12 -0400

    Add project files.

---
+++ CA_CommandArray/Program.cs
@@ -0,0 +1,210 @@
+﻿using FinchAPI;
+using System;
+
+namespace CommandArray
+{
+    // **************************************************************
+    // add comment block here
+    // **************************************************************
+
+    /// <summary>
+    /// control commands for the finch robot
+    /// </summary>
+    public enum FinchCommand
+    {
+        DONE,
+        MOVEFORWARD,
+        MOVEBACKWARD,
+        STOPMOTORS,
+        DELAY,
+        TURNRIGHT,
+        TURNLEFT,
+        LEDON,
+        LEDOFF
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Finch myFinch = new Finch();
+
+            DisplayOpeningScreen();
+            DisplayInitializeFinch(myFinch);
+            DisplayMainMenu(myFinch);
+            DisplayClosingScreen(myFinch);
+        }
+
+        /// <summary>
+        /// display the main menu
+        /// </summary>
+        /// <param name="myFinch">Finch object</param>
+        static void DisplayMainMenu(Finch myFinch)
+        {
+            string menuChoice;
+            bool exiting = false;
+
+            int delayDuration;
+            int numberOfCommands;
+            int motorSpeed;
+            int LEDBrightness;
+            FinchCommand[] commands;
+
+            while (!exiting)
+            {
+                //
+                // display menu
+                //
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("Main Menu");
+                Console.WriteLine();
+
+                Console.WriteLine("\t1) Get Command Parameters");
+                Console.WriteLine("\t2) ");
+                Console.WriteLine("\t3) ");
+                Console.WriteLine("\tE) Exit");
+                Console.WriteLine();
+                Console.Write("Enter Choice:");
+                menuChoice = Console.ReadLine();
+
+                //
+                // process menu
+                //
+                switch (menuChoice)
+                {
+                    case "1":
+                        numberOfCommands = DisplayGetNumberOfCommands();
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    case "e":
+                    case "E":
+                        exiting = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// get the number of commands from the user
+        /// </summary>
+        /// <returns>number of commands</returns>
+        static int DisplayGetNumberOfCommands()
+        {
+            int numberOfCommands;
+            string userResponse;
+
+            DisplayHeader("Number of Commands");
+
+            Console.Write("Enter the number of commands:");
+            userResponse = Console.ReadLine();
+
+            numberOfCommands = int.Parse(userResponse);
+
+            return numberOfCommands;
+        }
+
+        /// <summary>
+        /// initialize and confirm the finch connects
+        /// </summary>
+        /// <param name="myFinch"></param>
+        static void DisplayInitializeFinch(Finch myFinch)
+        {
+            DisplayHeader("Initialize the Finch");
+
+            Console.WriteLine("Please plug your Finch Robot into the computer.");
+            Console.WriteLine();
+            DisplayContinuePrompt();
+
+            while (!myFinch.connect())
+            {
+                Console.WriteLine("Please confirm the Finch Robot is connect");
+                DisplayContinuePrompt();
+            }
+
+            FinchConnectedAlert(myFinch);
+            Console.WriteLine("Your Finch Robot is now connected");
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// audio notification that the finch is connected
+        /// </summary>
+        /// <param name="myFinch">Finch object</param>
+        static void FinchConnectedAlert(Finch myFinch)
+        {
+            myFinch.setLED(0, 255, 0);
+
+            for (int frequency = 17000; frequency > 100; frequency -= 100)
+            {
+                myFinch.noteOn(frequency);
+                myFinch.wait(10);
+            }
+
+            myFinch.noteOff();
+        }
+
+        /// <summary>
+        /// display opening screen
+        /// </summary>
+        static void DisplayOpeningScreen()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\tProgram Your Finch");
+            Console.WriteLine();
+
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// display closing screen and disconnect finch robot
+        /// </summary>
+        /// <param name="myFinch">Finch object</param>
+        static void DisplayClosingScreen(Finch myFinch)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("\t\tThank You!");
+            Console.WriteLine();
+
+            myFinch.disConnect();
+
+            DisplayContinuePrompt();
+        }
+
+        #region HELPER  METHODS
+
+        /// <summary>
+        /// display header
+        /// </summary>
+        /// <param name="header"></param>
+        static void DisplayHeader(string header)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\t" + header);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// display the continue prompt
+        /// </summary>
+        static void DisplayContinuePrompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        #endregion
+    }
+}
